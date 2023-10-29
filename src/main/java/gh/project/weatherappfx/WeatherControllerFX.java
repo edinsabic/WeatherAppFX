e381542d89ab1175188204530eb14ec2427f6e81
edinsabic <sabic.edin29@gmail.com>
2023-10-29 17:48:51 +0100

    Dodana funkcionalnost prikaza vremena za določen kraj
    Še zmeraj je samo hardcodan seznam največjih 30 mest + par velik mest Slovenije

--- src/main/java/gh/project/weatherappfx/WeatherControllerFX.java
+++ src/main/java/gh/project/weatherappfx/WeatherControllerFX.java
@@ -30,53 +30,41 @@ public class WeatherControllerFX {
         String city = cityField.getText();
         welcomeText.setText("Searching for weather in " + city + "...");
 
-        try {
-            String weatherData = getWeatherDataForCity(city);
-            String weatherInfo = parseWeatherInfo(weatherData);
-
-            welcomeText.setText("Weather information for " + city + ":\n" + weatherInfo);
-        } catch (IOException e) {
-            e.printStackTrace();
-            welcomeText.setText("Error fetching weather data.");
-        }
+        String weatherData = getWeatherDataForCity(city);
+        String weatherInfo = parseWeatherInfo(weatherData);
+        welcomeText.setText("Weather information for " + city + ":\n" + weatherInfo);
     }
 
-    private String getWeatherDataForCity(String city) throws IOException {
-        String apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=46.0511&longitude=14.5051&current=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&timezone=Europe%2FBerlin";
-        URL url = new URL(apiUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder content = new StringBuilder();
-
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+    private String getWeatherDataForCity(String city) {
+        try {
+            return WeatherAPIfx.getWeatherData(city);
+        } catch (IOException | IllegalArgumentException e) {
+            e.printStackTrace();
+            return "Error fetching weather data.";
         }
-
-        in.close();
-        return content.toString();
     }
 
     private String parseWeatherInfo(String json) {
-        System.out.println(json);
         try {
             JSONObject jsonObject = new JSONObject(json);
             JSONObject current = jsonObject.getJSONObject("current");
             JSONObject daily = jsonObject.getJSONObject("daily");
 
             double temperatureValue = current.getDouble("temperature_2m");
+            double precipitationValue = current.getDouble("precipitation");
 
             int weatherCodeValue = daily.getJSONArray("weathercode").getInt(0);
             double maxDailyTemp = daily.getJSONArray("temperature_2m_max").getDouble(0);
             double minDailyTemp = daily.getJSONArray("temperature_2m_min").getDouble(0);
             double maxUVindex = daily.getJSONArray("uv_index_max").getDouble(0);
+
             String sunriseTime = daily.getJSONArray("sunrise").getString(0);
             String sunsetTime = daily.getJSONArray("sunset").getString(0);
 
-            return String.format("Current temperature: %.1f °C\nWeather Code: %d\nMax Temp: %.1f °C\nMin Temp: %.1f °C\nUV Index: %.1f\nSunrise: %s\nSunset: %s",
-                    temperatureValue, weatherCodeValue, maxDailyTemp, minDailyTemp, maxUVindex, sunriseTime, sunsetTime);
+            return String.format("Current temperature: %.1f °C\nPrecipitation: %.1f\nWeather Code: %d\n" +
+                            "Max Temp: %.1f °C\nMin Temp: %.1f °C\nUV Index: %.1f\nSunrise: %s\nSunset: %s",
+                    temperatureValue, precipitationValue, weatherCodeValue,
+                    maxDailyTemp, minDailyTemp, maxUVindex, sunriseTime, sunsetTime);
         } catch (JSONException e) {
             e.printStackTrace();
             return "N/A";
